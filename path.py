56fd4d010fb2ee58d116b7cad7b0bf2356365189
omri yakir <omriyakir@mail.tau.ac.il>
2024-07-30 01:36:43 +0300

    embeddings

--- path.py
+++ path.py
@@ -4,5 +4,7 @@ import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 datasets_path = os.path.join(current_dir, 'datasets')
 cpp_natural_residues_path = os.path.join(datasets_path, 'cpp_natural_residues')
+embeddings_path = os.path.join(datasets_path, 'embeddings')
 models_path = os.path.join(current_dir, 'models')
 results_path = os.path.join(current_dir, 'results')
+
